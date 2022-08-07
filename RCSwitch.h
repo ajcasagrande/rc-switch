ba7366bf5a83c55dc8b25e602995db9b319f3883
Anthony Casagrande <anthony.j.casagrande@intel.com>
2022-08-06 21:44:11 -0700

    fix: increase max changes to support longer codes

--- RCSwitch.h
+++ RCSwitch.h
@@ -58,7 +58,8 @@
 
 // Number of maximum high/Low changes per packet.
 // We can handle up to (unsigned long) => 32 bit * 2 H/L changes per bit + 2 for sync
-#define RCSWITCH_MAX_CHANGES 67
+// note: increased to support
+#define RCSWITCH_MAX_CHANGES 90
 
 class RCSwitch {
 
